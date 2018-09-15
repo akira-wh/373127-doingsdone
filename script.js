982fdaaa39a61c3b90854a02b15f58324a96998b
Keks <keks@htmlacademy.ru>
2018-09-15 17:24:37 +0300

    :cat:

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 'use strict';
 
+var expandControls = document.querySelectorAll('.expand-control');
+
 var hidePopups = function () {
   [].forEach.call(document.querySelectorAll('.expand-list'), function (item) {
     item.classList.add('hidden');
   });
 };
+
 document.body.addEventListener('click', hidePopups, true);
 
-var expandControls = document.querySelectorAll('.expand-control');
 [].forEach.call(expandControls, function (item) {
   item.addEventListener('click', function () {
     item.nextElementSibling.classList.toggle('hidden');
@@ -19,8 +21,8 @@ document.body.addEventListener('click', function (event) {
   var modal = null;
 
   if (target.classList.contains('open-modal')) {
-    var modalID = target.getAttribute('target');
-    modal = document.getElementById(modalID);
+    var modal_id = target.getAttribute('target');
+    modal = document.getElementById(modal_id);
 
     if (modal) {
       document.body.classList.add('overlay');
@@ -36,19 +38,23 @@ document.body.addEventListener('click', function (event) {
 });
 
 var $checkbox = document.getElementsByClassName('show_completed')[0];
+
 $checkbox.addEventListener('change', function (event) {
-  var isChecked = +event.target.checked;
-  window.location = '/index.php?show_completed=' + isChecked;
+  var is_checked = +event.target.checked;
+
+  window.location = '/index.php?show_completed=' + is_checked;
 });
 
 var $taskCheckboxes = document.getElementsByClassName('tasks')[0];
+
 $taskCheckboxes.addEventListener('change', function (event) {
   if (event.target.classList.contains('task__checkbox')) {
     var el = event.target;
-    var isChecked = +el.checked;
-    var taskID = el.getAttribute('value');
 
-    var url = '/index.php?task_id=' + taskID + '&check=' + isChecked;
+    var is_checked = +el.checked;
+    var task_id = el.getAttribute('value');
+
+    var url = '/index.php?task_id=' + task_id + '&check=' + is_checked;
     window.location = url;
   }
 });
