1bb756ae594adc1b96b0cfd73dda1b6483406897
Akira <154.4k1r4@gmail.com>
2018-10-01 01:28:34 +0600

    NEW: Данные пользователей перенесены из data.php в БД (queries.sql)

---
+++ queries.sql
@@ -0,0 +1,38 @@
+/**
+ * Моделирование таблицы пользователей.
+ * 5 пользователей.
+ */
+INSERT INTO users
+  SET name = 'Игнат',
+      email = 'ignat.v@gmail.com',
+      password = '$2y$10$OqvsKHQwr0Wk6FMZDoHo1uHoXd4UdxJG/5UDtUiie00XaxMHrW8ka',
+      registration = '2018-01-01 13:11',
+      contact_info = '';
+
+INSERT INTO users
+  SET name = 'Леночка',
+      email = 'kitty_93@li.ru',
+      password = '$2y$10$bWtSjUhwgggtxrnJ7rxmIe63ABubHQs0AS0hgnOo41IEdMHkYoSVa',
+      registration = '2018-03-08 15:50',
+      contact_info = '';
+
+INSERT INTO users
+  SET name = 'Руслан',
+      email = 'warrior07@mail.ru',
+      password = '$2y$10$2OxpEH7narYpkOT1H5cApezuzh10tZEEQ2axgFOaKW.55LxIJBgWW',
+      registration = '2018-05-25 11:50',
+      contact_info = '';
+
+INSERT INTO users
+  SET name = 'Акира',
+      email = 'doingsdone01@inbox.ru',
+      password = '$2y$10$3Y8bk7P7WGP005OpodF1/ebO.zMzTlHJPIUFslTKfH3uwk7v2T3SS', -- powerfulPassword13
+      registration = '2018-07-07 18:00',
+      contact_info = '';
+
+INSERT INTO users
+  SET name = 'Канэда',
+      email = 'doingsdone02@inbox.ru',
+      password = '$2y$10$3Y8bk7P7WGP005OpodF1/ebO.zMzTlHJPIUFslTKfH3uwk7v2T3SS', -- powerfulPassword13
+      registration = '2018-09-01 19:00',
+      contact_info = '';
