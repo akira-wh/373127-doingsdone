5fae8781b2a9b7fffdf4dcd747c18dbffaf1010a
Akira <154.4k1r4@gmail.com>
2018-10-03 13:15:51 +0600

    FIX: Исправлены типы данных и NOT NULL, поля camelCase (schema.sql)

--- schema.sql
+++ schema.sql
@@ -11,8 +11,8 @@ CREATE TABLE users (
   name          CHAR(64)    NOT NULL,
   email         CHAR(64)    NOT NULL,
   password      CHAR(255)   NOT NULL,
-  registration  DATETIME    DEFAULT CURRENT_TIMESTAMP,
-  contact_info  CHAR(255)   DEFAULT NULL,
+  registration  DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  contactInfo   CHAR(255)   DEFAULT NULL,
 
   PRIMARY KEY (id),
   UNIQUE INDEX (email)
@@ -20,25 +20,25 @@ CREATE TABLE users (
 
 /** Таблица категорий (проектов). */
 CREATE TABLE categories (
-  id          INT         AUTO_INCREMENT,
-  name        CHAR(255)   NOT NULL,
-  creator_id  INT         NOT NULL,
+  id         INT         AUTO_INCREMENT,
+  name       CHAR(255)   NOT NULL,
+  creatorID  INT         NOT NULL,
 
   PRIMARY KEY (id),
-  INDEX (creator_id)
+  INDEX (creatorID)
 );
 
 /** Таблица задач. */
 CREATE TABLE tasks (
-  id               INT          AUTO_INCREMENT,
-  name             CHAR(255)    NOT NULL,
-  category_id      INT          NOT NULL,
-  creation         DATETIME     DEFAULT CURRENT_TIMESTAMP,
-  deadline         DATETIME     DEFAULT NULL,
-  attachment_path  CHAR(255)    DEFAULT NULL,
-  is_complete      TINYINT(1)   DEFAULT '0', -- 1 || 0 (true || false)
+  id              INT          AUTO_INCREMENT,
+  name            CHAR(255)    NOT NULL,
+  categoryID      INT          NOT NULL,
+  creation        DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  deadline        DATETIME     DEFAULT NULL,
+  attachmentPath  CHAR(255)    DEFAULT NULL,
+  isComplete      TINYINT(1)   NOT NULL DEFAULT 0, -- 1 || 0 (true || false)
 
   PRIMARY KEY (id),
   INDEX (deadline),
-  INDEX (category_id)
+  INDEX (categoryID)
 );
