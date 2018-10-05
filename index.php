f0399671b3ad89109e4fe062b88d665af702cbcc
Akira <154.4k1r4@gmail.com>
2018-10-05 20:09:50 +0600

    NEW: Получение и рендер данных из БД (путем мультизапроса mysqli)

--- index.php
+++ index.php
@@ -18,42 +18,52 @@
   //
   /////////////////////////////////////////////////////////////////////////
 
+  // Соединение с БД.
+  $databaseConnection = mysqli_connect('doingsdone', 'root', '', 'doingsdone');
+  mysqli_set_charset($databaseConnection, 'uft8');
+
+  // Формирование запросов на получение списка категорий и задач из БД.
+  $requestForCategories = 'SELECT categories.id, categories.name FROM categories
+                          JOIN users ON categories.creator_id = users.id
+                          WHERE users.id = 4; ';
+
+  $requestForTasks = 'SELECT tasks.name,
+                              tasks.category_id,
+                              tasks.deadline,
+                              tasks.attachment_name,
+                              tasks.is_complete FROM tasks
+                      JOIN categories ON tasks.category_id = categories.id
+                      JOIN users ON categories.creator_id = users.id
+                      WHERE users.id = 4; ';
+
+  // Отправка запроса.
+  $request = mysqli_multi_query($databaseConnection, $requestForCategories.$requestForTasks);
+
+  // Получение, обработка и сохранение результата запроса.
+  $expectedResults = 2;
+  while ($expectedResults) {
+    mysqli_next_result($databaseConnection);
+    $receivedData = mysqli_store_result($databaseConnection);
+
+    if ($receivedData) {
+      switch ($expectedResults) {
+        case 2:
+          $categories = mysqli_fetch_all($receivedData, MYSQLI_ASSOC);
+          break;
+        case 1:
+          $tasks = mysqli_fetch_all($receivedData, MYSQLI_ASSOC);
+          break;
+      }
+    }
+
+    $expectedResults--;
+  }
+
   // Получение названия страницы.
   $pageTitle = PAGES_TITLES['index'];
 
   // Сборка header.
   $pageHeader = fillView(VIEWS['siteHeader']);
-
-  // Открытие соединения с БД.
-  $databaseConnection = mysqli_connect('doingsdone', 'root', '', 'doingsdone');
-  mysqli_set_charset($databaseConnection, 'uft8');
-
-  // Формирование запроса на получение списка категорий из БД.
-  $requestString = 'SELECT categories.id, categories.name FROM categories
-                    JOIN users ON categories.creator_id = users.id
-                    WHERE users.id = 4';
-
-  // Отправка запроса.
-  $requestForCategories = mysqli_query($databaseConnection, $requestString);
-
-  // Парсинг полученных данных из БД.
-  $categories = mysqli_fetch_all($requestForCategories, MYSQLI_ASSOC);
-
-  // Формирование запроса на получение списка задач из БД.
-  $requestString = 'SELECT tasks.name,
-                            tasks.category_id,
-                            tasks.deadline,
-                            tasks.attachment_name,
-                            tasks.is_complete FROM tasks
-                    JOIN categories ON tasks.category_id = categories.id
-                    JOIN users ON categories.creator_id = users.id
-                    WHERE users.id = 4';
-
-  // Отправка запроса.
-  $requestForTasks = mysqli_query($databaseConnection, $requestString);
-
-  // Парсинг полученных данных из БД.
-  $tasks = mysqli_fetch_all($requestForTasks, MYSQLI_ASSOC);
 
   // Сборка sidebar (список категорий).
   $pageSidebar = fillView(VIEWS['sidebarCategories'], [
