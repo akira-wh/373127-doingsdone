ad4cbc17c72a0e99cc031d8f6eb54a802f1a6b78
Akira <154.4k1r4@gmail.com>
2018-10-16 13:18:58 +0600

    IMP: Из проекта вычищен мусор (неиспользуемые классы, скрипты)

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
 'use strict';
 
+// var hidePopups = function () {
+//   [].forEach.call(document.querySelectorAll('.expand-list'), function (item) {
+//     item.classList.add('hidden');
+//   });
+// };
+// document.body.addEventListener('click', hidePopups, true);
+
+// var expandControls = document.querySelectorAll('.expand-control');
+// [].forEach.call(expandControls, function (item) {
+//   item.addEventListener('click', function () {
+//     item.nextElementSibling.classList.toggle('hidden');
+//   });
+// });
+
+// document.body.addEventListener('click', function (event) {
+//   var target = event.target;
+//   var modal = null;
+//
+//   if (target.classList.contains('open-modal')) {
+//     var modalID = target.getAttribute('target');
+//     modal = document.getElementById(modalID);
+//
+//     if (modal) {
+//       document.body.classList.add('overlay');
+//       modal.removeAttribute('hidden');
+//     }
+//   }
+//
+//   if (target.classList.contains('modal__close')) {
+//     modal = target.parentNode;
+//     modal.setAttribute('hidden', 'hidden');
+//     document.body.classList.remove('overlay');
+//   }
+// });
+
 var updateURLParameter = function (url, parameter, value) {
-  // index.php?one=1&two=2
   url = String(url);
 
-  // one=1&two=2
   var urlParameters = url.split('?')[1];
 
   if (!urlParameters) {
@@ -21,41 +54,6 @@ var updateURLParameter = function (url, parameter, value) {
     return parameter + '=' + value;
   });
 };
-
-var hidePopups = function () {
-  [].forEach.call(document.querySelectorAll('.expand-list'), function (item) {
-    item.classList.add('hidden');
-  });
-};
-document.body.addEventListener('click', hidePopups, true);
-
-var expandControls = document.querySelectorAll('.expand-control');
-[].forEach.call(expandControls, function (item) {
-  item.addEventListener('click', function () {
-    item.nextElementSibling.classList.toggle('hidden');
-  });
-});
-
-document.body.addEventListener('click', function (event) {
-  var target = event.target;
-  var modal = null;
-
-  if (target.classList.contains('open-modal')) {
-    var modalID = target.getAttribute('target');
-    modal = document.getElementById(modalID);
-
-    if (modal) {
-      document.body.classList.add('overlay');
-      modal.removeAttribute('hidden');
-    }
-  }
-
-  if (target.classList.contains('modal__close')) {
-    modal = target.parentNode;
-    modal.setAttribute('hidden', 'hidden');
-    document.body.classList.remove('overlay');
-  }
-});
 
 var $showCompletedTasksCheckbox = document.getElementsByClassName('show_completed')[0];
 $showCompletedTasksCheckbox.addEventListener('change', function (event) {
@@ -79,9 +77,9 @@ $tasksCheckboxes.addEventListener('change', function (event) {
   }
 });
 
-flatpickr('#date', {
-  enableTime: true,
-  dateFormat: "Y-m-d H:i",
-  time_24hr: true,
-  locale: "ru"
-});
+// flatpickr('#date', {
+//   enableTime: true,
+//   dateFormat: "Y-m-d H:i",
+//   time_24hr: true,
+//   locale: "ru"
+// });
